a7d754afcfd2256fcbeb7b34b82fbdf9e09a6236
guru <gururaj.konded@gmail.com>
2025-03-26 14:15:54 +0530

    initial commit

---
+++ src/main/java/com/example/empdeptapp/EmpDepAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.empdeptapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpDepAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpDepAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/empdeptapp/EmpDepAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empdeptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpDepAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
