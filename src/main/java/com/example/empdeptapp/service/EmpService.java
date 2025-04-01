853447c3c03190ed788e0490e972f5fd32527aa1
guru <gururaj.konded@gmail.com>
2025-04-01 19:27:39 +0530

    bugfixing in emp service

--- src/main/java/com/example/empdeptapp/service/EmpService.java
+++ src/main/java/com/example/empdeptapp/service/EmpService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.empdeptapp.entity.Dept;
 import com.example.empdeptapp.entity.Emp;
+import com.example.empdeptapp.repo.DeptRepository;
 import com.example.empdeptapp.repo.EmpRepository;
 
 @Service
@@ -14,14 +17,23 @@ public class EmpService {
 	@Autowired
 	private EmpRepository empRepository;
 	
+	@Autowired
+	private DeptRepository deptRepository;
+	
 	public List<Emp> getEmployees(){
 		
 		return empRepository.findAll();
 			
 	}
 	
+	@Transactional
 	public Emp createEmployee(Emp emp) {
 		
+		Long deptId =emp.getDept().getId();
+		
+		Dept dept=deptRepository.findById(deptId).orElseThrow(()-> new RuntimeException("department not found"));
+		
+		emp.setDept(dept); 
 		return empRepository.save(emp);
 		
 	} 
